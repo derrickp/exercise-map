bb461fc0fc086f9dedffb6ccbbc0e3e3de3eebdf
Derrick Plotsky <derrickplotsky@gmail.com>
2018-01-06 20:27:13 -0800

    mapbox start

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
 
+import mapboxgl from "mapbox-gl";
 
-alert("Woohoo!");
+mapboxgl.accessToken = "pk.eyJ1IjoiZHBsb3Rza3kiLCJhIjoiY2pjNDIzbHR3MTM1eTMwbnIwbzkzanV1NCJ9.zyryj72jwNEV2xFmH1shGQ";
+
+const map = new mapboxgl.Map({
+    container: 'exercise-map',
+    style: 'mapbox://styles/mapbox/streets-v9'
+});
